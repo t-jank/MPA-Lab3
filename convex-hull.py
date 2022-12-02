3812ba8f98790d730b1dd925b15b4fd64d032704
TomaszJankowiak <62778369+TomaszJankowiak@users.noreply.github.com>
2022-12-02 20:16:34 +0100

    jarvis_march on going

--- convex-hull.py
+++ convex-hull.py
@@ -8,8 +8,67 @@ import random
 import matplotlib.pyplot as plt
 import math
 
+def length(A,B):
+    return math.sqrt( (A[0]-B[0])**2 + (A[1]-B[1])**2 )
 
-n = 100
+
+def angle(A,B,C):
+    if B==C: return -1
+    return math.acos( (length(A,B)**2 + length(B,C)**2 - length(A,C)**2) / (2*length(A,B)*length(B,C)) )
+
+
+def update_angles(hull,points,angles):
+    hull_size=len(hull)
+    for i in range(0,len(points)):
+        angles[i]=angle(hull[hull_size-2], hull[hull_size-1], points[i])
+
+
+def Jarvis_march(points):
+    
+    hull=[]
+    hull.append([0,0])
+    hull.append([0,9999])
+    for i in range(0,len(points)): # finding first point of hull, the smallest y
+        if points[i][1]<hull[1][1]:
+            hull[1] = points[i]
+    hull[0]=([-9999,hull[1][1]]) # point zero [-inf, y(P1)]
+    
+    angles=[]
+    for i in range(0,len(points)):
+        angles.append([0])
+    update_angles(hull,points,angles)
+        
+    N=([-1,-1])
+    while(True):
+        N=points[0]
+        angle=-1
+        for i in range(0,len(points)):
+            if angles[i]>angle:
+                angle=angles[i]
+                N=points[i]    
+        if N==hull[1]:
+            break
+        hull.append(N)
+        update_angles(hull,points,angles)
+    
+    
+    hull.append(points[3])
+    hull.append(points[5])
+    hull.append(points[8])
+    
+    
+    hull_x=[]
+    hull_y=[]
+    for i in range(1,len(hull)):
+        hull_x.append(hull[i][0])
+        hull_y.append(hull[i][1])
+    hull_x.append(hull_x[0])
+    hull_y.append(hull_y[0])
+    return hull_x, hull_y
+
+
+
+n = 10
 plane = 'c' # square or circle
 Points=[]
 
@@ -34,4 +93,7 @@ for i in range(0,len(Points)):
 plt.axis('square')
 plt.xlim([0,n])
 plt.ylim(0,n)
+#Jarvis_march(Points)
+hullxy=Jarvis_march(Points)
+plt.plot(hullxy[0],hullxy[1])
 plt.show()
